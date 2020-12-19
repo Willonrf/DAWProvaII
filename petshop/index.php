fe0a22309e1e863fd63490fa3a120d408f55cb72
Willonrf <willonrf@gmail.com>
2020-12-19 00:23:55 -0300

    Criação do projeto

---
+++ petshop/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Petshop</title>
+    </head>
+    <body>
+        <!-- Na raiz do projeto, deve existir uma página
+        com dois links, um para o cadastro de
+        espécie e outro para o cadastro de animal. -->
+    </body>
+</html>
